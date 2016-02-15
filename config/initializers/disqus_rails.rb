28989b07f841a355f2548b6e5986497634d48dbd
Nazariy Papizh <nazariy.papizh@gmail.com>
2016-02-15 18:40:28 +0200

    changed disqus remote server

--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -1,5 +1,5 @@
 DisqusRails.setup do |config|
-  config::SHORT_NAME = "valkoua"
+  config::SHORT_NAME = "valkoukraine"
   config::SECRET_KEY = "your_secret_disqus_key" #leave blank if not used
   config::PUBLIC_KEY = "your public_disqus_key" #leave blank if not used
   config::ACCESS_TOKEN = "your_access_token" #you got it, right? ;-)
