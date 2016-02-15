b96ee1376ac21d516d1445b6053ef20e64b1b1ea
Nazariy Papizh <nazariy.papizh@gmail.com>
2016-02-15 18:16:51 +0200

    ready static data

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -3,4 +3,7 @@ module MainHelper
   def promotions
     @promotions = Promotion.all
   end
+  def static_data
+    return data = StaticDatum.first
+  end
 end
--- app/models/static_datum.rb
+++ app/models/static_datum.rb
@@ -11,6 +11,42 @@ class StaticDatum < ActiveRecord::Base
 
 
   rails_admin do
+    navigation_label 'Інші'
 
+    label 'Статичні дані'
+    label_plural 'Статичні дані'
+    edit do
+      group :address do
+        label 'Адреса'
+        field :city do
+          label 'Місто:'
+        end
+        field :street do
+          label 'Вулиця:'
+        end
+      end
+      group :phone_numbers do
+        label 'Номери телефонів'
+        field :tel1 do
+          label 'Номер стаціонарного:'
+        end
+        field :tel2 do
+          label 'Номер мобільного:'
+        end
+      end
+      group :socials do
+        label 'Лінки на соціальні мережі'
+        active false
+        field :facebook do
+          label 'facebook:'
+        end
+        field :youtube do
+          label 'youtube:'
+        end
+        field :google do
+          label 'google:'
+        end
+      end
+    end
   end
 end
