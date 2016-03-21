0fe7c337af68e60bb4e7fc455b0753bbc548f861
Nazariy Papizh <nazariy.papizh@gmail.com>
2016-03-21 14:10:12 +0200

    decking constroller requests

--- app/controllers/roof_railing_controller.rb
+++ app/controllers/roof_railing_controller.rb
@@ -11,26 +11,35 @@ class RoofRailingController < ApplicationController
   end
   def get_rr_options
     received_key = params[:key]
-    option_value = received_key.partition('-').first
+    producer = received_key.partition('-').first
     product = received_key.partition('-').last
 
     decking = RoofRailItem.find_by_slug(product)
 
+    # all producers by product
     producer_by_item = RoofRailItem.where(slug: product).pluck(:producer).uniq
-    thickness_by_producer = RoofRailItem.where(slug: product).where(producer: option_value).pluck(:thickness).uniq
-    thickness_arr = RoofRailItem.thickness.values.map { |thickness| thickness if thickness.in?(thickness_by_producer) }
 
-    coating_by_thickness = RoofRailItem.where(slug: product).where(producer: option_value).where(thickness: thickness_arr.select{|i| !i.nil? }[0]).pluck(:coating).uniq
-    coating_arr = RoofRailItem.coating.values.map { |coating| coating if coating.in?(coating_by_thickness) }
+    # all thickness by producer
+    thickness_by_producer = RoofRailItem.where(slug: product).where(producer: producer).pluck(:thickness).uniq
+    thickness_arr = thickness_by_producer.map { |thickness| thickness}
+    # thickness_arr = RoofRailItem.thickness.values.map { |thickness| thickness if thickness.in?(thickness_by_producer) }
 
-    protective_lamina_by_coating = RoofRailItem.where(slug: product).where(producer: option_value).where(thickness: thickness_arr[0]).where(coating: coating_arr.select{|i| !i.nil? }[0]).pluck(:protective_lamina).uniq
-    protective_lamina_arr = RoofRailItem.protective_lamina.values.map { |protective_lamina| protective_lamina if protective_lamina.in?(protective_lamina_by_coating) }
+    # all coating by first thickness
+    coating_by_thickness = RoofRailItem.where(slug: product).where(producer: producer).where(thickness: thickness_arr.select{|i| !i.nil? }[0]).pluck(:coating).uniq
+    coating_arr = coating_by_thickness.map { |coating| coating }
+    # coating_arr = RoofRailItem.coating.values.map { |coating| coating if coating.in?(coating_by_thickness) }
 
+    # all lamina by first coating
+    protective_lamina_by_coating = RoofRailItem.where(slug: product).where(producer: producer).where(thickness: thickness_arr[0]).where(coating: coating_arr[0]).pluck(:protective_lamina).uniq
+    protective_lamina_arr = protective_lamina_by_coating.map { |protective_lamina| protective_lamina }
+    # protective_lamina_arr = RoofRailItem.protective_lamina.values.map { |protective_lamina| protective_lamina if protective_lamina.in?(protective_lamina_by_coating) }
 
+    # all colors by selected options
+    test_item = RoofRailItem.where(slug: product).where(producer: producer).where(thickness: thickness_arr[0]).where(coating: coating_arr[0]).first
+    colors_arr = test_item.rr_details.map {|color| color.title}
 
-    options = {thickness: thickness_arr, coating: coating_arr, protective_lamina: protective_lamina_arr}.to_json
-
-    temp = 0
+    # json string with data
+    options = {thickness: thickness_arr, coating: coating_arr, protective_lamina: protective_lamina_arr, colors: colors_arr }.to_json
 
     respond_to do |format|
       format.json { render :json => options }
