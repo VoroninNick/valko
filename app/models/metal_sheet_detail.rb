69f5293d5fb1dc5593d038d7129b7a832964237f
Nazariy <nazariy.papizh@gmail.com>
2016-09-12 13:16:09 +0300

    ready one item metal sheet page

--- app/models/metal_sheet_detail.rb
+++ app/models/metal_sheet_detail.rb
@@ -94,4 +94,25 @@ class MetalSheetDetail < ActiveRecord::Base
     (((width.to_f/1000) * 1) * color_options.first.price).round()
     # (((item.try(&:width).to_f/1000) * 1) * item.rr_details.first.price).round()
   end
+
+  def present_colors
+    self.color_options.pluck(:title).uniq
+  end
+
+  def producers_by_item
+    MetalSheetDetail.where(title: self.title).pluck(:producer).uniq
+  end
+
+  def thickness_by_producers(producer)
+    MetalSheetDetail.where(title: self.title).where(producer: producer).pluck(:thickness).uniq
+  end
+
+  def coating_by_thickness
+    MetalSheetDetail.where(title: self.title).where(producer: self.producer).where(thickness: self.thickness).pluck(:coating).uniq
+  end
+
+  def protective_lamina_by_coating
+    MetalSheetDetail.where(title: self.title).where(producer: self.producer).where(thickness: self.thickness).where(coating: self.coating).pluck(:protective_lamina).uniq
+  end
+
 end
