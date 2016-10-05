d33ab7582a18d104e1cd84ce4ca6aa7e2c994270
Nazariy <nazariy.papizh@gmail.com>
2016-10-05 14:05:04 +0300

    testing metal tile

--- app/controllers/roof_railing_controller.rb
+++ app/controllers/roof_railing_controller.rb
@@ -171,6 +171,10 @@ class RoofRailingController < ApplicationController
     end
   end
 
+
+
+
+
   def get_metal_tile_options
 
     product_key = params[:key]
@@ -190,37 +194,43 @@ class RoofRailingController < ApplicationController
       params_type = 'lamina'
     end
 
-    product = MetalTileDetail.where(slug: product_key)
+    # decking = RoofRailItem.find_by_slug(product)
+    #
+    # # all producers by product
+    # producer_by_item = RoofRailItem.where(slug: decking.slug).pluck(:producer).uniq
+
+    product = MetalTileDetail.find_by_slug(product_key)
+    products = MetalTileDetail.where(slug: product.slug)
 
     # all producers by product
-    producer_by_item = product.pluck(:producer).uniq
+    producer_by_item = products.map(&:producer).uniq
 
     # all thickness by producer
-    thickness_by_producer = product.where(producer: producer).pluck(:thickness).uniq
+    thickness_by_producer = products.where(producer: producer).pluck(:thickness).uniq
     thickness_arr = thickness_by_producer.map { |thickness| thickness}
 
     # all coating by first thickness
-    coating_by_thickness = product.where(producer: producer).where(thickness: thickness_arr.select{|i| !i.nil? }[0]).pluck(:coating).uniq
+    coating_by_thickness = products.where(producer: producer).where(thickness: thickness_arr.select{|i| !i.nil? }[0]).pluck(:coating).uniq
     coating_arr = coating_by_thickness.map { |coating| coating }
 
     # all lamina by first coating
-    protective_lamina_by_coating = product.where(producer: producer).where(thickness: thickness_arr.select{|i| !i.nil? }[0]).where(coating: coating_arr.select{|i| !i.nil? }[0]).pluck(:protective_lamina).uniq
+    protective_lamina_by_coating = products.where(producer: producer).where(thickness: thickness_arr.select{|i| !i.nil? }[0]).where(coating: coating_arr.select{|i| !i.nil? }[0]).pluck(:protective_lamina).uniq
     protective_lamina_arr = protective_lamina_by_coating.map { |protective_lamina| protective_lamina }
 
     # all colors by selected options
-    test_item = product.where(producer: producer).where(thickness: thickness_arr.select{|i| !i.nil? }[0]).where(coating: coating_arr.select{|i| !i.nil? }[0]).first
+    test_item = products.where(producer: producer).where(thickness: thickness_arr.select{|i| !i.nil? }[0]).where(coating: coating_arr.select{|i| !i.nil? }[0]).first
     colors_arr = test_item.color_options.map {|color| {title: color.title, price: color.price, image: color.image.url(:thumb), image_large: color.image.url(:large)} }
 
     # json string with data
     if params_type == 'producer'
       product_by_producer = MetalTileDetail.find_by_producer(producer_by_item[0])
-      current_producer = product.where(producer: producer)
+      current_producer = products.where(producer: producer)
 
       # options to send
       options = {thickness: thickness_arr, coating: coating_arr, protective_lamina: protective_lamina_arr, colors: colors_arr, product_id: current_producer.first.id}.to_json
 
     elsif params_type == 'thickness'
-      current_thickness = product.where(producer: producer).where(thickness: thickness)
+      current_thickness = products.where(producer: producer).where(thickness: thickness)
       coating_by_thickness = current_thickness.pluck(:coating).uniq
       coating_arr = coating_by_thickness.map { |coating| coating }
 
@@ -233,7 +243,7 @@ class RoofRailingController < ApplicationController
       options = {coating: coating_arr, protective_lamina: protective_lamina_arr, colors: colors_arr, product_id: current_thickness.first.id }.to_json
 
     elsif params_type == 'coating'
-      current_el = product.where(producer: producer).where(thickness: thickness).where(coating: coating)
+      current_el = products.where(producer: producer).where(thickness: thickness).where(coating: coating)
       protective_lamina_by_coating = current_el.pluck(:protective_lamina).uniq
       protective_lamina_arr = protective_lamina_by_coating.map { |protective_lamina| protective_lamina }
 
@@ -244,7 +254,7 @@ class RoofRailingController < ApplicationController
       options = {protective_lamina: protective_lamina_arr, colors: colors_arr, product_id: current_el.first.id }.to_json
 
     elsif params_type == 'lamina'
-      current_lamina = product.where(producer: producer).where(thickness: thickness).where(coating: coating).where(protective_lamina: lamina)
+      current_lamina = products.where(producer: producer).where(thickness: thickness).where(coating: coating).where(protective_lamina: lamina)
       colors_arr = current_lamina.first.color_options.map {|color| {title: color.title, price: color.price, image: color.image.url(:thumb), image_large: color.image.url(:large)} }
 
       # options to send
