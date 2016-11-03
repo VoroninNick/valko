eeac6688945b9769ffdbb9970c633f9bbd64a781
Nazariy <nazariy.papizh@gmail.com>
2016-11-03 16:23:07 +0200

    t

--- db/migrate/20161103112656_create_join_table_choicest_item_information.rb
+++ db/migrate/20161103112656_create_join_table_choicest_item_information.rb
@@ -1,5 +1,5 @@
 class CreateJoinTableChoicestItemInformation < ActiveRecord::Migration
   def change
-    create_join_table :choicest_items, :informations
+    create_join_table :choicest_items, :information
   end
 end
